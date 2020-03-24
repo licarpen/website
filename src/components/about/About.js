b816f06bbbe0f2f506106960b7a6e36abc5265ef
Lisa Carpenter <carpenter.lisa@gmail.com>
2020-03-23 21:41:51 -0700

    add content to About and mobile responsiveness

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import styles from './About.css';
 
 const About = () => (
-  <div>About</div>
+  <div className={styles.About}>
+    <p>Lisa Carpenter is a software engineer based in Portland, Oregon.  She absolutely loves to code.  Her expertise in physics and mathematics enables her to craft logical, eloquent solutions to complex problems.  She writes readable, tested code and is in search of an incredible team.</p>
+    <br/>
+    <p>When not coding up a storm, Lisa can be found playing jazz trumpet, advocating for bike/ped/scooter infrastructure, sharing physics demonstrations with friends and family, and exploring the outdoors through rock climbing, hiking, and biking.</p>
+  </div>
 );
 
 

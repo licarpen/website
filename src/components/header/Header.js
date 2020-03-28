c9ab934ba288fc67c547df5c812b55759c901623
Lisa Carpenter <carpenter.lisa@gmail.com>
2020-03-28 15:59:49 -0700

    change header into home Link

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import styles from './Header.css';
 import NavigationBar from '../navigation/NavigationBar';
+import { Link } from 'react-router-dom';
 
 const Header = () => (
   <div className={styles.Header}>
     <NavigationBar className={styles.NavigationBar}/>
-    <h1>CARPENTER CODE</h1>
-    <h2>Code By Lisa Carpenter</h2>
+    <Link className={styles.Link} to={'/'}>
+      <h1>CARPENTER CODE</h1>
+      <h2>Code By Lisa Carpenter</h2>
+    </Link>
   </div>
 );
 
